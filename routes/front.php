d4227952d1796d330ac9ddf788d7a93ffd7ec093
Mirai2699 <ian26balatbat@gmail.com>
2021-02-07 15:31:50 +0800

    Fixed refresh issue

--- routes/front.php
+++ routes/front.php
@@ -14,10 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 // Add an appropriate middleware for API @TODO
+//
+//Route::get('/', 'HomePageController@viewMainPage');
+////Route::post('/login', 'LoginPageController@login');
+////Route::post('/register', 'RegistrationPageController@register');
+//Route::get('/vue/{vue_capture?}', function () {
+//    return view('vue.index');
+//   })->where('vue_capture', '[\/\w\.-]*');
 
-Route::get('/', 'HomePageController@viewMainPage');
-Route::post('/login', 'LoginPageController@login');
-Route::post('/register', 'RegistrationPageController@register');
-Route::get('/vue/{vue_capture?}', function () {
-    return view('vue.index');
-   })->where('vue_capture', '[\/\w\.-]*');
+Route::get('/{any}', function () {
+    return view('main');
+})->where('any', '.*');
