9ff4d327a5f046dcc4b8a1fd1e6265a065609a2b
Mirai2699 <ian26balatbat@gmail.com>
2021-04-03 17:48:29 +0800

    Test deploy

--- config/app.php
+++ config/app.php
@@ -163,6 +163,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Laravel\Lumen\Http\ResponseFactory::class,
 
         /*
          * Package Service Providers...
