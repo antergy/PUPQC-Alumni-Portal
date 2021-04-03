e21a49f4b68677a28987a5b81d422505447b1726
Mirai2699 <ian26balatbat@gmail.com>
2021-04-03 16:29:43 +0800

    Test deploy

--- routes/front.php
+++ routes/front.php
@@ -16,34 +16,37 @@ use Illuminate\Support\Facades\Route;
 // Add an appropriate middleware for API @TODO
 
 /** NOTE: It is important that the vue routes are different from the web routes */
-$aVueRoutes = [
-    '/',
-    '/home'
-];
-foreach ($aVueRoutes as $sRoute) {
-    Route::get($sRoute, function () use ($sRoute) {
-        /** Retrieve session from backend */
-        $aSession = \App\Libraries\Common\AuthLib::getUserSession();
-        $aSessionStatus = data_get($aSession, 'user_session_active', null);
-
-        /** Condition to check if session is active or not */
-        if ($aSessionStatus === null) {
-            if ($sRoute === '/') { // If the session is dead and attempts to go to main home page or other page
-                return view('main');
-            } else {
-                return redirect('/');
-            }
-        } else { // If the session is active and attempted to return to login page
-            if ($sRoute === '/') {
-                return redirect('/home');
-            } else {
-                return view('main');
-            }
-        }
-    })->where('any', '.*');
-}
-
-Route::post('/login', 'AuthController@login');
-Route::post('/logout', 'AuthController@logout');
-Route::get('/getSession', 'AuthController@getSession');
+Route::get('/', function () {
+    return 'test';
+});
+//$aVueRoutes = [
+//    '/',
+//    '/home'
+//];
+//foreach ($aVueRoutes as $sRoute) {
+//    Route::get($sRoute, function () use ($sRoute) {
+//        /** Retrieve session from backend */
+//        $aSession = \App\Libraries\Common\AuthLib::getUserSession();
+//        $aSessionStatus = data_get($aSession, 'user_session_active', null);
+//
+//        /** Condition to check if session is active or not */
+//        if ($aSessionStatus === null) {
+//            if ($sRoute === '/') { // If the session is dead and attempts to go to main home page or other page
+//                return view('main');
+//            } else {
+//                return redirect('/');
+//            }
+//        } else { // If the session is active and attempted to return to login page
+//            if ($sRoute === '/') {
+//                return redirect('/home');
+//            } else {
+//                return view('main');
+//            }
+//        }
+//    })->where('any', '.*');
+//}
+//
+//Route::post('/login', 'AuthController@login');
+//Route::post('/logout', 'AuthController@logout');
+//Route::get('/getSession', 'AuthController@getSession');
 
