431638150cfb80b2808717cbd1b47e2240a3e4d8
Mirai2699 <ian26balatbat@gmail.com>
2021-04-03 14:34:14 +0800

    updated bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -26,10 +26,10 @@ $app = new Illuminate\Foundation\Application(
 |
 */
 
-//$app->singleton(
-//    Illuminate\Contracts\Http\Kernel::class,
-//    App\Http\Kernel::class
-//);
+$app->singleton(
+    Illuminate\Contracts\Http\Kernel::class,
+    App\Http\Kernel::class
+);
 
 $app->singleton(
     Illuminate\Contracts\Console\Kernel::class,
